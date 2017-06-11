b960fdf43741861a25b3640ab969ce00fbb7be60
ashishghumnar <ashish.ghumnar@gmail.com>
2017-06-11 17:54:47 +0530

    added server events

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var express = require('express'),
     morgan = require('morgan'),
     bodyParser = require('body-parser');
 
+var server = require('http').Server(app);
+var socketClient = require('socket.io-client')('http://localhost:9002');
+var io = require('socket.io')(server);
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9004,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-app.use(express.static('views'));
 
+app.use(express.static('views'));
 
 // error handling
 app.use(function (err, req, res, next) {
@@ -19,7 +23,19 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Something bad happened!');
 });
 
-app.listen(port, ip);
+socketClient.on('connect', function (socketClient) {
+    console.log(socketClient);
+});
+
+socketClient.on('switchToggle', function (data) {
+    socketChannel.emit('switchToggleOnUI', data);
+});
+
+io.on('connection', function (socket) {
+    socketChannel = socket;
+});
+
+server.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app;
--- views/app.js
+++ views/app.js
@@ -8,4 +8,14 @@ AppCtrl.$inject = ['$scope', '$http'];
 
 function AppCtrl($scope, $http) {
     var vm = this;
+
+    vm.deviceStatus = "OFF";
+
+    var socket = io.connect('http://localhost:9004');
+
+    socket.on('switchToggleOnUI', function (data) {
+        $scope.$apply(function () {
+            vm.deviceStatus = data.deviceStatus;
+        });
+    });
 }
